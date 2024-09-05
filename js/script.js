7d340233aaec0c682e3f687c241e861ae106cefc
surefire-hu <surefire.hu2001@gmail.com>
2024-09-05 17:40:56 +0200

    un po' di ordine

--- js/script.js
+++ js/script.js
@@ -246,7 +246,11 @@ const app = createApp({
         },
         riceviMessaggioAutomatico() {
             const now = new Date(); 
-            const formattedDate = `${now.getDate().toString().padStart(2, '0')}/${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getFullYear()} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
+            const formattedDate = `${now.getDate().toString().padStart(2, '0')}/
+            ${(now.getMonth() + 1).toString().padStart(2, '0')}/
+            ${now.getFullYear()} ${now.getHours().toString().padStart(2, '0')}:
+            ${now.getMinutes().toString().padStart(2, '0')}:
+            ${now.getSeconds().toString().padStart(2, '0')}`;
             
             if (this.contacts[this.contattoSelezionato].online === true) {
                 this.contacts[this.contattoSelezionato].messages.push({
